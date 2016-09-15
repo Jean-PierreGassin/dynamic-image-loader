58d6f5289ae9b9f09372831b960823948831f0ce
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2016-09-15 22:22:56 +1000

    Update indentation to PSR standards

--- content/image_loader.php
+++ content/image_loader.php
@@ -7,7 +7,7 @@ $image = isset($_GET['image']) ? $_GET['image'] : false;
 
 // Check if our params are set
 if (!$image) {
-	return (new Response(null, 404))->send();
+    return (new Response(null, 404))->send();
 }
 
 // Set the location of the image
@@ -15,13 +15,13 @@ $imagePath = 'images/' . $image . '.jpg';
 
 // Check if our image exists at this location and return an image, else return 404
 if (file_exists($imagePath)) {
-	$fp = fopen($imagePath, 'r');
-	$stream = stream_get_contents($fp);
+    $fp = fopen($imagePath, 'r');
+    $stream = stream_get_contents($fp);
 
-	return (new Response($stream, 200, [
-		'Content-Type' => 'image/jpg',
-		'Content-Length' => filesize($imagePath)
-		]))->send();
-} else {
-	return (new Response(null, 404))->send();
-}
+    return (new Response($stream, 200, [
+        'Content-Type' => 'image/jpg',
+        'Content-Length' => filesize($imagePath)
+        ]))->send();
+    } else {
+        return (new Response(null, 404))->send();
+    }
